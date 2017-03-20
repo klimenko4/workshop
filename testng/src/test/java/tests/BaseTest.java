d668088173a28a4270d4f551951919853c1b1d3f
sergey <sergio_89@ukr.net>
2017-03-20 15:43:59 +0200

    video support added to testng

--- testng/src/test/java/tests/BaseTest.java
+++ testng/src/test/java/tests/BaseTest.java
@@ -1,5 +1,9 @@
 package tests;
 
+import com.automation.remarks.testng.VideoListener;
+import com.automation.remarks.video.enums.RecordingMode;
+import com.automation.remarks.video.enums.VideoSaveMode;
+import com.automation.remarks.video.recorder.VideoRecorder;
 import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.testng.annotations.BeforeClass;
@@ -11,7 +15,7 @@ import tests.utils.TestContainersListener;
  * Created by sergey on 19.03.17.
  */
 //@Docker(image = "spirogov/video_service:latest", exposePort = 8086)
-@Listeners(TestContainersListener.class)
+@Listeners({TestContainersListener.class, VideoListener.class})
 public class BaseTest {
 
     @BeforeClass
@@ -19,5 +23,10 @@ public class BaseTest {
         ChromeDriverManager.getInstance().setup();
         Configuration.browser = "chrome";
         //Configuration.holdBrowserOpen = true;
+        VideoRecorder.conf()
+                .withVideoFolder("./build/video")
+                .withRecordMode(RecordingMode.ALL)
+                .withVideoSaveMode(VideoSaveMode.ALL);
+
     }
 }
